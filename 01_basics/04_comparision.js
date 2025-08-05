250c41cd883448a64275de2c9fc14d0e1c8e1753
imvikasdwivedi <vikas.gnnps7@gmail.com>
2025-08-05 20:51:25 +0000

    Comparision basics

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,6 @@
+console.log("2">1)// converts string to num then comares
+console.log("2"<1)
+console.log("2"==2)
+console.log("2"===2) //false bcoz strict comp. also compares date types.
+// generally not a good practice and not recommended to compare diff data types
+

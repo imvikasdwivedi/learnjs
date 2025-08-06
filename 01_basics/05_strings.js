4e78afc00d23cd4fb6d6860aa929fde5bce2d3db
imvikasdwivedi <vikas.gnnps7@gmail.com>
2025-08-06 21:12:41 +0000

    basics operations on strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+const str1 = "Encyclopedia"
+const string2 = "Vikas"
+const str3='The-name-is-Bond'
+const repocount = 50
+console.log(`My name is ${string2} and my repocount is ${repocount}`);
+console.log(str1.slice(-7,8));
+console.log(str1.charAt(4));
+console.log(str1.indexOf('p'));
+console.log(str1.toUpperCase());
+console.log(str1.includes('clop'));
+console.log(str1.replace('o','u'));
+console.log(str3.split('-'));
+console.log(string2.split(''));
+console.log(string2.length);
+
+
+
+
+
+
+
+

c2c4380208aeba60f24c7802641bd88a890375e6
imvikasdwivedi <vikas.gnnps7@gmail.com>
2025-08-25 17:54:00 +0000

    objects accessing props

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -3,5 +3,21 @@
 //object literals
 
 const JsUser={
-    name : 'Vikas'
+    name : 'Vikas',
+    'full Name': 'Vikas Dwivedi',
+    age : 18,
+    location : 'Noida',
+    email :'vikas@openai.com',
+    isLoggedIn: false,
+    lastLoginDays : ['Monday','Wednesday']    
 }
+console.log(JsUser.email)
+console.log(JsUser['email']);
+//console.log(JsUser.full Name); this will not function because
+//  when prop. key of object is in double quotes this won't work.
+console.log(JsUser['full Name']);// always works 
+console.log(JsUser.location);
+
+
+
+
